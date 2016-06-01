8c8c9f320fbc916d41a2e2a7b3626b031ab143ec
Tyler Woods <tyler.woods66@gmail.com>
2016-06-01 11:27:25 -0500

    fix foundation

--- tango.php
+++ tango.php
@@ -1,9 +1,11 @@
 <?php
-
 namespace Grav\Theme;
 
 use Grav\Common\Theme;
 
-class Tango extends Theme {
+class Tango extends Theme
+{
+    // Boostrap plugin will look for this class var to know it should load
+    public $load_bootstrapper_plugin = true;
 
 }
